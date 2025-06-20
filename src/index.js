e34f81013f8abf308cde8bec82b35c6eace6d48c
harsh-coder-desgin <harshpatel51057@gmail.com>
2025-06-20 16:07:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,18 @@
-// require('dotenv').config({path:'/env'})
-import { app } from "./app.js"
-// import cors from 'cors';
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
-console.log("second");
-
+import {app} from './app.js'
 dotenv.config({
-    path:'./.env'
-}) 
+    path: './.env'
+})
 
-// app.use(cors({
-//   origin: 'https://aurcode.vercel.app', // or ['http://localhost:5173', 'https://your-frontend.vercel.app']
-//   credentials: true, // only if using cookies
-// }));
+
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log(`server is runing at port : ${process.env.PORT}`);
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
-    app.on("error",(error)=>{
-            console.log("error",error);
-            throw error
-        })
 })
-.catch((err)=>{
-    console.log("mongo db connection failed",err)
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
